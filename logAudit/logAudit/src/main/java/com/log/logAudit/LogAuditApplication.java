66113c130a4faaa4e66980105f53c0028221a853
carlos.gomes <carlos.gomes@castgroup.com.br>
2022-05-07 16:20:50 -0300

    initial commit

---
+++ logAudit/logAudit/src/main/java/com/log/logAudit/LogAuditApplication.java
@@ -0,0 +1,13 @@
+package com.log.logAudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAuditApplication.class, args);
+	}
+
+}
